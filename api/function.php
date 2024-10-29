1489cd94e83364dc1db99013a1f10143b33c213c
Andre-231401040 <andrelim865@gmail.com>
2024-10-29 15:53:21 +0700

    coba

--- api/function.php
+++ api/function.php
@@ -1,10 +1,9 @@
 <?php 
 $host = "ep-damp-waterfall-a13tat59-pooler.ap-southeast-1.aws.neon.tech";
 $user = "default";
-$port = "5432";
 $password = "PfrRKpVY73zU";
 $dbname = "verceldb";
-$con = pg_connect("host=$host port=$port user=$user password=$password dbname=$dbname");
+$con = pg_connect("host=$host user=$user password=$password dbname=$dbname");
 if(!$con){
     die("Koneksi ke Database Gagal.");
 }
